1a0c644729e37f9b692620ae12f0a0f33348080e
aldon4ik <39241905+aldon4ik@users.noreply.github.com>
2018-07-21 13:38:22 +0300

    second test

---
+++ src/test/java/Second/SecondTest.java
@@ -0,0 +1,24 @@
+package Second;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondTest {
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        // Optional, if not specified, WebDriver will search your path for chromedriver.
+        final String s = System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+}
